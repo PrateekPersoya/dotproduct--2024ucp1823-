fbf7942c358c163f6e32594f2d76f9b95cab4fdb
sidharthseethala-exe <2024ucp1823@mnit.ac.in>
2025-11-17 15:25:20 +0530

    Intial commit with a readme and vecadd

---
+++ README.md
@@ -0,0 +1,2 @@
+This is dot product py
+we are undrestading how to coolab and git properties
---
+++ vecadd.py
@@ -0,0 +1,22 @@
+import numpy as np
+#<<<<<<< HEAD
+A = np.array([1,2,3,4,5]) 
+#=======
+#A = np.array([1,2,3,4])
+#>>>>>>> parallel
+C = np.array([1,2,3])
+dot = np.dot(A,C)
+D = np.array([1,3,5])
+# E = np.array([1,7,5])
+E = np.array([1,5,7])
+# fixed the bug by correcly entering the E
+dot = np.dot(A,E)
+print(dot)
+
+# fast computing
+def compute_dot(A, B):
+    return np.dot(A, B)
+    
+print(f"Tesiting the stash")
+#testing the stash again
+
